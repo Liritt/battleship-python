76823af550e4d175a90083133a606fb97ba5a356
Gaétan Lemont <gaetan.lemont@etudiant.univ-reims.fr>
2022-01-05 21:20:13 +0100

    Ecriture de la fonction placerBateauxManuel

--- main.py
+++ main.py
@@ -5,22 +5,18 @@
 
 import pygame
 
-from view import window
-
+from model.Manuel import placerBateauxManuel
 from model.Constantes import *
 from model.Joueur import construireJoueur
+from view import window
 
 def main_test():
-    j = construireJoueur("Test", [const.PORTE_AVION, const.CUIRASSE, const.CROISEUR, const.TORPILLEUR])
-    # j = construireJoueur("Test", [const.PORTE_AVION, const.CUIRASSE])
-    window.afficher(j)
-    window.placer_bateaux()
-    window.set_action("Pour terminer, cliquez dans la grille de DROITE")
-    window.get_clicked_cell(2)
+ j = construireJoueur("Galérien", [const.PORTE_AVION, const.CUIRASSE,
+ const.CROISEUR, const.TORPILLEUR])
+ placerBateauxManuel(j)
+ window.set_action("Pour terminer, cliquez dans la grille de DROITE")
+ window.get_clicked_cell(2)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main_test()
-
+if __name__ == '__main__' :
+ main_test()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- model/Manuel.py
+++ model/Manuel.py
@@ -1,3 +1,18 @@
 # model/Manuel.py
 #
+from view import window
+from model.Joueur import getNomJoueur, type_joueur, construireJoueur
+
+def placerBateauxManuel(joueur: dict) -> None:
+    if not type_joueur(joueur):
+        raise ValueError(f"reinitialiserBateauxJoueur: {joueur} n'est pas un joueur")
+    window.afficher(joueur)
+    window.display_message(f"{getNomJoueur(joueur)} : placez vos bateaux")
+    window.placer_bateaux()
+
+
+
+
+
+
 
